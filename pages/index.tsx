5fa4db4071ca4905e8ceff0c2f0765f355b7b0ce
Cindy Atrihana <cindy.atrihana@gmail.com>
2022-07-14 17:25:44 +0700

    create react-query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
 import Head from 'next/head'
 import {ReactQueryCacheProvider, QueryCache, useQuery} from 'react-query'
+import {ReactQueryDevtools} from 'react-query-devtools'
 
 import { ArticleCollection } from '../components/ArticleCollection'
 
+const queryCache = new QueryCache()
+const toJSON = (_: Response) => _.json()
+const fetcher = () => fetch('https://jsonplaceholder.typicode.com/posts').then(toJSON)
+
 export default function Home() {
+
+  const { data: articles, isLoading, error } = useQuery('articles', fetcher);
+
+  if (isLoading) return 'Loading...'
+  if (error) return error.message;
+  
   return (
-    <ReactQueryCacheProvider>
-      <div className='max-w-md mx-auto mt-4'>
-        <ArticleCollection/>
-      </div>
-    </ReactQueryCacheProvider>
-  )
+    <>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <div className="max-w-md mx-auto mt-4">
+          <ArticleCollection collection={articles} />
+        </div>
+      </ReactQueryCacheProvider>
+      <ReactQueryDevtools />
+    </>
+  );
 }
